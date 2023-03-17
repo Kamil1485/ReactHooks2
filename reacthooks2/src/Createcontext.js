1968136f9cddad47ef3b02cc5197efe701787f14
Kamil1485 <81561240+Kamil1485@users.noreply.github.com>
2023-03-17 23:15:57 +0300

    Update Createcontext.js

--- reacthooks2/src/Createcontext.js
+++ reacthooks2/src/Createcontext.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { createContext,useState } from 'react';
 
 const CountContext=createContext();
-//3-Normalde hookslar funcrtion compennet  içinde asagıda olmalı 
 //4-CreateContext  kodları karısık bundan kurtulmak içi useContext() kullanıyoruz
 
 const CreateContext = () => {
  
-    // ! const{Provider,Consumer}=createContext();
- //2.Olusturma sekli tercih edilmez,birden fazla context olusturursan herbirine Provider,Consumer yazarsan karışıklık olur 1.yöntemi bil!!
+  // ! const{Provider,Consumer}=createContext();
  //1
- // ^2 adet context olustrudum,2.si createcontext ten dönen nesneleri tek tek aldım ilkinde icinde
-// ^1.si CountContext.Provider= 2.si=Provider yani!!
+ // ^2 adet context olustrudum,2.si createcontext ten dönen nesneleri tek tek aldım,
 
 //2 
 //provider:Bir değişkeni oluşturup o değişkeni istediğimiz yerde consumer ile alıp kullanabilme
